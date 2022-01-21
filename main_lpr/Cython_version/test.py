5dc74e9adb4cac240ead311f7fd5df6f6a95d132
halogass <halogas.ijul@gmail.com>
2022-01-22 00:25:11 +0700

    merapikan file tidak terpakai

--- main_lpr/Cython_version/test.py
+++
@@ -1,8 +0,0 @@
-import arsalpr as ai
-import cv2
-
-
-imgPathList = ['../platnomor.jpg', '../platnomor1.png', '../platnomor2.png', '../platnomor3.png']
-for imgPath in imgPathList:
-    gambarIn = cv2.imread(imgPath)
-    ai.tesProses(gambarIn)
--- main_lpr_openvino/Cython_version/setup_compile.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-files = ["arsai.pyx", "arsalpr.pyx"]
-
-setup(
-    ext_modules=cythonize(files),
-    zip_safe=False,
-)
